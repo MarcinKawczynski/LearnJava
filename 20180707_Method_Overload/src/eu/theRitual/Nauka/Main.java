2b7c3dd9e74abcb2851f53cc5d1f4db27ecdd11d
Marcin Kawczyński <zielony.mowi@gmail.com>
2018-07-29 13:09:15 +0200

    Check Update

--- 20180707_Method_Overload/src/eu/theRitual/Nauka/Main.java
+++ 20180707_Method_Overload/src/eu/theRitual/Nauka/Main.java
@@ -3,18 +3,16 @@ package eu.theRitual.Nauka;
 public class Main {
 
     public static void main(String[] args) {
-	wypiz ("Marcin",2000);
-	wypiz ("Józek", 1500);
-	wypiz (1400);
+        wypiz("Marcin", 2000);
+        wypiz("Józek", 1500);
+        wypiz(1400);
     }
 
-    public static void wypiz(String nick, int wynik)
-    {
+    public static void wypiz(String nick, int wynik) {
         System.out.println(nick + " uzyskał " + wynik + " punktów.");
     }
 
-    public static void wypiz(int wynik)
-    {
+    public static void wypiz(int wynik) {
         System.out.println("Nieznany Gracz uzyskał " + wynik + " punktów.");
     }
 }
